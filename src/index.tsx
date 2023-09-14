20a3771085d26dc157863c0f663cbed2b2cab693
Eyelly Wu <eyelly.wu@gmail.com>
2023-09-14 21:45:11 +0800

    build(deps): update @i18n-pro/solid to the latest

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import { render } from 'solid-js/web'
-import { Provider } from '@i18n-pro/solid'
+import { I18nProvider } from '@i18n-pro/solid'
 import i18nState from './i18n'
 import App from './App'
 
 render(
   () => (
-    <Provider {...i18nState}>
+    <I18nProvider {...i18nState}>
       <App />
-    </Provider>
+    </I18nProvider>
   ),
   document.getElementById('root') as HTMLElement
 )
